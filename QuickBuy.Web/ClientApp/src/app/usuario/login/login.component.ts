6a899353f821a6d57e8a872b280b7f54298c7085
Fabio Lazari Costa <lazari.developer@gmail.com>
2021-06-12 18:17:22 -0300

    Validações campos

--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Usuario } from "../../model/usuario";
 
 @Component({
   selector: "app-login",
@@ -7,8 +8,24 @@ import { Component } from "@angular/core";
 })
 
 export class LoginComponent {
+  public usuario;
+  //public usuarioAutenticado: boolean;
+
+  //public usuarios = ["usuario1", "usuario2", "usuario3", "usuario4", "usuario5"];
+
+  constructor() {
+    this.usuario = new Usuario();
+  }
+
+  entrar() {
+    if (this.usuario.email == "fabio@gmail.com" && this.usuario.senha == "123456") {
+
+    }
+     // this.usuarioAutenticado = true;
+  }
 
 }
+
 
 
 
