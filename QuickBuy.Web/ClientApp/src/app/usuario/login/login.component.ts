038923c113b99f3074b7be64f6c492f73c1139e0
Fabio Lazari Costa <lazari.developer@gmail.com>
2021-06-13 20:43:08 -0300

    Ajustando navegação login/sair

--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,6 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Usuario } from "../../model/usuario";
-import { Router } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: "app-login",
@@ -8,19 +8,24 @@ import { Router } from "@angular/router";
   styleUrls: ["./login.component.css"]
 })
 
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   public usuario;
+  public returnUrl: string;
   //public usuarioAutenticado: boolean;
   //public usuarios = ["usuario1", "usuario2", "usuario3", "usuario4", "usuario5"];
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+  }
+
+  ngOnInit() : void {
+    this.returnUrl = this.activatedRoute.snapshot.queryParams['returnUrl'];
     this.usuario = new Usuario();
   }
 
   entrar() {
     if (this.usuario.email == "fabio@gmail.com" && this.usuario.senha == "123456") {
       sessionStorage.setItem("usuario-autenticado", "1");
-      this.router.navigate(['/']);
+      this.router.navigate([this.returnUrl]);
     }
   }
 }
