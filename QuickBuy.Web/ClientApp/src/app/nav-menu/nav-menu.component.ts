dcca6cbb3aff5f8a0035b4ab5808f81dea7818c4
Fabio Lazari Costa <lazari.developer@gmail.com>
2021-06-30 07:18:35 -0300

    Ajustes carrinho compra

--- QuickBuy.Web/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ QuickBuy.Web/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { LojaCarrinhoCompras } from '../loja/carrinho-compras/loja.carrinho.compras';
 import { UsuarioServico } from '../servicos/usuario/usuario.servico';
 
 @Component({
@@ -7,8 +8,10 @@ import { UsuarioServico } from '../servicos/usuario/usuario.servico';
   templateUrl: './nav-menu.component.html',
   styleUrls: ['./nav-menu.component.css']
 })
-export class NavMenuComponent {
+export class NavMenuComponent implements OnInit {
+
   isExpanded = false;
+  public carrinhoCompra: LojaCarrinhoCompras;
 
   get usuario() {
     return this.usuarioServico.usuario;
@@ -17,6 +20,11 @@ export class NavMenuComponent {
   constructor(private router: Router, private usuarioServico: UsuarioServico) {
 
   }
+
+  ngOnInit(): void {
+    this.carrinhoCompra = new LojaCarrinhoCompras();
+  }
+
   collapse() {
     this.isExpanded = false;
   }
@@ -46,4 +54,9 @@ export class NavMenuComponent {
     //sessionStorage.setItem("usuario-autenticado", "");
     this.router.navigate(['/']);
   }
+
+  public temItensCarrinhoCompras(): boolean {
+    return this.carrinhoCompra.temItensCarrinhoCompras();
+  }
+
 }
